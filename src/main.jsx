6023f84a400dbee9d85cff591c1c069db78cf081
DavidMdez <mdez.hdez.gamaliel@gmail.com>
2023-06-30 12:18:17 -0600

    add material and struture

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 
 import { JournalApp } from './JournalApp'
 import './styless.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <JournalApp />
+    <BrowserRouter>
+      <JournalApp />
+    </BrowserRouter>
   </React.StrictMode>,
 )
