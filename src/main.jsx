b5cc84ec840d9b5ef01ddd370bfc745316008383
DavidMdez <mdez.hdez.gamaliel@gmail.com>
2023-07-16 13:25:06 -0600

    production

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import { JournalApp } from './JournalApp'
@@ -10,9 +10,9 @@ import './styless.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store } >
-      <BrowserRouter>
+      <HashRouter>
         <JournalApp />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
