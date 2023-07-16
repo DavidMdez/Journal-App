76f36c262e21a035a6c3692982d71fe226e1a84c
DavidMdez <mdez.hdez.gamaliel@gmail.com>
2023-07-16 14:08:39 -0600

    routes config

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -21,7 +21,7 @@ export const AppRouter = () => {
         : <Route path="auth/*" element={ <AuthRoutes /> } />
       }
 
-      <Route path='/*' element={ <Navigate to='/auth/login' /> } />
+      <Route path='/*' element={ <Navigate to='/auth/login' replace /> } />
     </Routes>
   )
 }
