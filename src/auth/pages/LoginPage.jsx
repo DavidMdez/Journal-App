857880db4980fe6dea5a88d78a5466a02e9742e6
DavidMdez <mdez.hdez.gamaliel@gmail.com>
2023-07-22 20:21:45 -0600

    test login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -39,7 +39,10 @@ export const LoginPage = () => {
 
   return (
     <AuthLayout title="Login">
-      <form onSubmit={ onSubmit } className="animate__animated animate__fadeIn animate__faster">
+      <form 
+        aria-label="submit-form"
+        onSubmit={ onSubmit }
+        className="animate__animated animate__fadeIn animate__faster">
         <Grid container>
           <Grid item xs={ 12 } sx={{ mt: 2 }}>
             <TextField 
@@ -56,6 +59,9 @@ export const LoginPage = () => {
 
           <Grid item xs={ 12 } sx={{ mt: 2 }}>
             <TextField 
+              inputProps={{ 
+                'data-testid': 'password' 
+              }}
               label="Contraseña"
               type="password"
               placeholder="Contraseña"
@@ -93,6 +99,7 @@ export const LoginPage = () => {
                 disabled={ isAuthenticating }
                 variant="contained" 
                 fullWidth
+                aria-label="google-btn"
                 onClick={ onGoogleSingIn }
               >
                 <Google />
--- tests/auth/pages/LoginPage.test.jsx
+++ tests/auth/pages/LoginPage.test.jsx
@@ -1,20 +1,41 @@
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { MemoryRouter } from "react-router-dom";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import { LoginPage } from "../../../src/auth/pages/LoginPage";
 import { authSlice } from "../../../src/store/auth";
+import { notAuthenticatedState } from "../../fixtures/authFixtures";
+
+const mockStartGoogleSingIn = jest.fn();
+const mockStartLoginWithEmailPassword = jest.fn();
+
+jest.mock('../../../src/store/auth/thunks', () => ({
+  startGoogleSingIn: () => mockStartGoogleSingIn,
+  startLoginWithEmailPassword: ({ email, password }) => {
+    return () => mockStartLoginWithEmailPassword({ email, password });
+  }
+}));
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => (fn) => fn(),
+}));
 
 describe('Prueba en <Login Page />', () => {
+
+  beforeEach(() => jest.clearAllMocks());
 
   const store = configureStore({
     reducer: {
       auth: authSlice.reducer
     },
+    preloadedState: {
+      auth: notAuthenticatedState
+    }
   });
 
-  test('debe de mostrar el componente correctamente', () => { 
+  test('debe de mostrar el componente correctamente', () => {
 
     render(
       <Provider store={store} >
@@ -22,9 +43,51 @@ describe('Prueba en <Login Page />', () => {
           <LoginPage />
         </MemoryRouter>
       </Provider>
-    )
+    );
 
     // screen.debug();
-    expect( screen.getAllByText('Login').length ).toBeGreaterThanOrEqual(1);
+    expect(screen.getAllByText('Login').length).toBeGreaterThanOrEqual(1);
+  });
+
+  test('boton de google deve de llamar el startGoogleSignIn', () => {
+
+    render(
+      <Provider store={store} >
+        <MemoryRouter>
+          <LoginPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const googleBtn = screen.getByLabelText('google-btn');
+    fireEvent.click(googleBtn);
+
+    expect(mockStartGoogleSingIn).toHaveBeenCalled();
+  });
+
+  test('submit del formulario debe de llamar el startLoginWithEmailPassword', () => {
+    
+    const email = 'davia@gmail.com';
+    const password = '123456';
+
+    render(
+      <Provider store={store} >
+        <MemoryRouter>
+          <LoginPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const emailField = screen.getByRole('textbox', { name: 'Correo' });
+    fireEvent.change(emailField, { target: { name: 'email', value: email } });
+
+    const passwordField = screen.getByTestId('password');
+    fireEvent.change(passwordField, { target: { name: 'password', value: password } });
+
+    const loginForm = screen.getByLabelText('submit-form');
+    fireEvent.submit(loginForm);
+
+    expect(mockStartLoginWithEmailPassword).toHaveBeenCalledWith({ email, password });
+
   });
 });
